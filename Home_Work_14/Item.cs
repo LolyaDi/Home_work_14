9d7baf6892739328e65f5a20589e25b9584abf56
LolyaDi <you@example.com>
2019-03-30 22:24:42 +0600

    Четырнадцатая домашняя работа

---
+++ Home_Work_14/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Home_Work_14
+{
+    public class Item
+    {
+        public string Title { get; set; }
+
+        public string Link { get; set; }
+
+        public string Description { get; set; }
+
+        public string PublicDate { get; set; }
+
+        public Item(string title, string link, string description, string publicDate)
+        {
+            Title = title;
+            Link = link;
+            Description = description;
+            PublicDate = publicDate;
+        }
+
+        public Item()
+        {
+
+        }
+    }
+}
---
+++ Home_Work_14/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Home_Work_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument XmlDocument = new XmlDocument();
+            const string URL_ADDRESS = "https://habr.com/ru/rss/interesting/";
+
+            XmlDocument.Load(URL_ADDRESS);
+            List<Item> items = new List<Item>();
+
+            foreach (XmlNode titleNode in XmlDocument.SelectNodes(@"/rss/channel/item"))
+            {
+                string title = titleNode.SelectSingleNode("title").InnerText;
+
+                string link = titleNode.SelectSingleNode("link").InnerText;
+
+                string description = titleNode.SelectSingleNode("description").InnerText;
+
+                string publicDate = titleNode.SelectSingleNode("pubDate").InnerText;
+
+                items.Add(new Item(title, link, description, publicDate));
+            }
+
+            foreach (Item item in items)
+            {
+                Console.WriteLine($"Title - {item.Title}\n");
+                Console.WriteLine($"Link - {item.Link}\n");
+                Console.WriteLine($"Description - {item.Description}\n");
+                Console.WriteLine($"PubDate - {item.PublicDate}\n\n\n");
+            }
+
+            XmlDocument secondXmlDocument = new XmlDocument();
+
+            XmlElement students = secondXmlDocument.CreateElement("Students");
+            secondXmlDocument.AppendChild(students);
+
+            XmlElement student = secondXmlDocument.CreateElement("Student");
+            student.SetAttribute("FullName", "Yerik Dilnaz");
+            student.SetAttribute("Hometown", "Nur-Sultan(Astana)");
+            student.SetAttribute("University", "Moscow State University");
+
+
+            Console.WriteLine("---------------------------------------------------");
+            students.AppendChild(student);
+            Console.WriteLine($"Ф.И.О - {secondXmlDocument.DocumentElement.FirstChild.Attributes["FullName"].Value}\n");
+            Console.WriteLine($"Родной город - {secondXmlDocument.DocumentElement.FirstChild.Attributes["Hometown"].Value}\n");
+            Console.WriteLine($"Университет - {secondXmlDocument.DocumentElement.FirstChild.Attributes["University"].Value}");
+            Console.ReadKey();
+        }
+    }
+}
